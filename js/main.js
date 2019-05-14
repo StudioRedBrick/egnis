471b2c13ecc49a2ad720ef4695e40cb1fcf8ab9f
sungyeeun <sungye727@gmail.com>
2019-05-14 15:26:02 +0900

    yeeun

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){ 
-
+    
+    instructor();
+    
+    
 }); //document ready END 
+
+
+function instructor(){
+    // flexslider
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        slideshow:true,
+        itemWidth: 210,
+        itemMargin: 160,
+        minItems: 2,
+        maxItems: 2,
+        move:1,
+//        end : function(slider){
+//                $('.flexslider .slides li').each(function(){ slider.addSlide('<li>'+j$(this).context.innerHTML+'</li>', slider.count);
+//                    jQuery('.flexslider .slides').append('<li>'+jQuery(this).context.innerHTML+'</li>');
+//                });
+//            }
+      });
+}
