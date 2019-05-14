9f26351510d70e64e6c74e22891eb66e5c875661
subin <sbyoon92@gmail.com>
2019-05-14 15:46:36 +0900

    subin commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,61 @@
 $(document).ready(function(){ 
 
+    faqAco();
 }); //document ready END 
+
+function faqAco(){
+    
+    var count1 = 0,
+        count2 = 0,
+        count3 = 0;
+    
+    $(".box6 .aco .highlight").on('click', function(){
+        //close all accordion
+        $(".box6 .aco .txt").stop().animate({"height":"0px","opacity":"0","transform":"scaleY(0%)"},300);
+        
+        var i = $(this).parent().index()+1;
+
+        if(i == 1){
+            count1++;
+            count2 = 0;
+            count3 = 0;
+            var remain1 = count1%2;
+            
+            if(remain1 == 1){
+            //open accordion
+                $(".box6 .aco1 .txt").stop().animate({"height":"105px","opacity":"0.8","transform":"scaleY(100%)"},300);
+            }else{
+            //close accordion
+                $(".box6 .aco1 .txt").stop().animate({"height":"0px","opacity":"0","transform":"scaleY(0%)"},300);
+            }
+        }else if(i == 2){
+            count2++;
+            count1 = 0;
+            count3 = 0;
+            var remain2 = count2%2;
+
+            if(remain2 == 1){
+            //open accordion
+                $(".box6 .aco2 .txt").stop().animate({"height":"105px","opacity":"0.8","transform":"scaleY(100%)"},300);
+            }else{
+            //close accordion
+                $(".box6 .aco2 .txt").stop().animate({"height":"0px","opacity":"0","transform":"scaleY(0%)"},300);
+            }
+        }else if(i == 3){
+            count3++;
+            count1 = 0;
+            count2 = 0;
+            var remain3 = count3%2;
+
+            if(remain3 == 1){
+            //open accordion
+                $(".box6 .aco3 .txt").stop().animate({"height":"105px","opacity":"0.8","transform":"scaleY(100%)"},300);
+            }else{
+            //close accordion
+                $(".box6 .aco3 .txt").stop().animate({"height":"0px","opacity":"0","transform":"scaleY(0%)"},300);
+            }
+        }
+        //console.log('remain1= '+remain1+' remain2= '+remain2+' remain3= '+remain3);
+        //console.log('count1= '+count1+' count2= '+count2+' count3= '+count3);
+    });
+}
